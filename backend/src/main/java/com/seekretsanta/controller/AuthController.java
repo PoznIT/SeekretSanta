8948da5a8eb9cffc4c717716ca5431996d04c0b7
pozniako16 <lev.pozniakoff@gmail.com>
2025-10-05 14:49:42 +0200

    hardcoded cross origin

--- backend/src/main/java/com/seekretsanta/controller/AuthController.java
+++ backend/src/main/java/com/seekretsanta/controller/AuthController.java
@@ -9,8 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = {"http://localhost:3000", "http://83.228.210.115:3000"})
 public class AuthController {
 
     private final UserService userService;
